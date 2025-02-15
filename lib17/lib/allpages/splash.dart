ca0064c04dae2293f4cff71abff579ee145f013e
Pujan-Ajmera <160005231+Pujan-Ajmera@users.noreply.github.com>
2025-02-15 12:51:44 +0530

    Add files via upload

---
+++ lib17/lib/allpages/splash.dart
@@ -0,0 +1,68 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+import 'dashboard.dart';
+
+class Splash extends StatefulWidget {
+  const Splash({super.key});
+
+  @override
+  State<Splash> createState() => _SplashState();
+}
+
+class _SplashState extends State<Splash> {
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    _navigateToHome();
+  }
+
+  _navigateToHome() async{
+    await Future.delayed(Duration(milliseconds: 5000),() {});
+    Navigator.pushReplacement(context, MaterialPageRoute(builder: (context)=>Dashboard(),));
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.black,
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Icon(
+              Icons.favorite,
+              color: Colors.pinkAccent,
+              size: 100,
+            ),
+            SizedBox(height: 20),
+            Center(
+              child: Text(
+                "Matrimony Connect",
+                style: GoogleFonts.poppins(
+                  fontSize: 25,
+                  fontWeight: FontWeight.w500,
+                  color: Colors.white,
+                ),
+              ),
+            ),
+            SizedBox(height: 10),
+            Text(
+              "Finding Your Perfect Match",
+              style: GoogleFonts.poppins(
+                fontSize: 18,
+                color: Colors.grey,
+              ),
+            ),
+            SizedBox(height: 40),
+            SpinKitWave(
+              color: Colors.pinkAccent,
+              size: 50.0,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
